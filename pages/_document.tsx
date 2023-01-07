2264e710ebc1b61cdcb58f12f824b8e64110db8d
mvciek <mpeciakk@gmail.com>
2023-01-07 20:18:00 +0100

    eslint errors fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" />
-      <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'></link>
+      <link href='https://fonts.googleapis.com/css?family=Open Sans&display=optional' rel='stylesheet' />
       <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
       <body>
         <Main />
